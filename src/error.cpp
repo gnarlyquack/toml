cc8906288a4696df9c6c05e3992f3a77cd249ee1
Karl Nack <karl.nack@fastmail.com>
2023-10-12 20:45:24 -0500

    Relicense under the BSD Zero Clause License

--- src/error.cpp
+++ src/error.cpp
@@ -1,3 +1,8 @@
+// This file is part of toml. It is subject to the license terms in the LICENSE
+// file found in the top-level directory of this distribution. No part of this
+// project, including this file, may be copied, modified, propagated, or
+// distributed except according to the terms contained in the LICENSE file.
+
 #include "error.hpp"
 
 #include <iomanip>
--- src/token.hpp
+++ src/token.hpp
@@ -1,7 +1,7 @@
-//  This file is part of toml-parser. It is subject to the license terms in the
-//  LICENSE file found in the top-level directory of this distribution. No part
-//  of this project, including this file, may be copied, modified, propagated,
-//  or distributed except according to the terms contained in the LICENSE file.
+// This file is part of toml. It is subject to the license terms in the LICENSE
+// file found in the top-level directory of this distribution. No part of this
+// project, including this file, may be copied, modified, propagated, or
+// distributed except according to the terms contained in the LICENSE file.
 
 #ifndef TOML_TOKEN_HPP
 #define TOML_TOKEN_HPP
